fefa3efbcb1e84792c179f35deec84064636be08
ian-codes <72500207+ian-codes@users.noreply.github.com>
2022-08-23 08:49:20 +0200

    Add files via upload

---
+++ out/_next/static/um4VisC4TaaVLK5kXiXmQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/456-52c0cf36e4dd1eb5.js","static/css/dbfb0ee90efac77d.css","static/chunks/pages/index-7f6f57c96e62eadb.js"],"/_error":["static/chunks/pages/_error-607d02b82bdbd270.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
